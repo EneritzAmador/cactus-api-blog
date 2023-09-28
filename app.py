68f078dfb012e2994b7910cde7fa629d10cd30c9
Ene AL <kaixo.eneal@gmail.com>
2023-09-28 18:47:03 +0200

    fix app.py

--- app.py
+++ app.py
@@ -4,18 +4,16 @@ from decouple import config
 from flask_cors import CORS
 import os
 
-
 SECRET_KEY = config('SECRET_KEY')
 FLASK_ENV = config('FLASK_ENV')
 
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b7yg6313a3fkrrhl:hrwtway2sa03k18a@uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vsl7sgj741ptkwil'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.secret_key = SECRET_KEY
 
 CORS(app)
 
 db = SQLAlchemy(app)
-
 
 # Define tu modelo de datos (por ejemplo, para un blog)
 class BlogPost(db.Model):
@@ -23,22 +21,6 @@ class BlogPost(db.Model):
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.String(1000), nullable=False)
     image = db.Column(db.String(255))
-
-# Base de datos temporal para pruebas
-blogs = [
-    {
-        "id": 1,
-        "title": "Primer Entrada",
-        "content": "Contenido de la primera entrada",
-        "image": "imagen1.jpg"
-    },
-    {
-        "id": 2,
-        "title": "Segunda Entrada",
-        "content": "Contenido de la segunda entrada",
-        "image": "imagen2.jpg"
-    }
-]
 
 # Ruta para una página de bienvenida
 @app.route('/welcome', methods=['GET'])
@@ -48,15 +30,26 @@ def welcome():
 # Ruta para obtener todas las entradas
 @app.route('/getblogs', methods=['GET'])
 def get_blogs():
-    return jsonify(blogs)
+    blogs = BlogPost.query.all()
+    return jsonify([{
+        "id": blog.id,
+        "title": blog.title,
+        "content": blog.content,
+        "image": blog.image
+    } for blog in blogs])
 
 # Ruta para obtener una entrada por su ID
 @app.route('/getblogs/<int:blog_id>', methods=['GET'])
 def get_blog(blog_id):
-    blog = next((blog for blog in blogs if blog['id'] == blog_id), None)
+    blog = BlogPost.query.get(blog_id)
     if blog is None:
         return jsonify({'message': 'Blog no encontrado'}), 404
-    return jsonify(blog)
+    return jsonify({
+        "id": blog.id,
+        "title": blog.title,
+        "content": blog.content,
+        "image": blog.image
+    })
 
 # Ruta para crear una nueva entrada
 @app.route('/create_blog', methods=['POST'])
@@ -73,38 +66,53 @@ def create_blog():
     else:
         image_path = None
 
-    new_blog = {
-        'id': len(blogs) + 1,
-        'title': title,
-        'content': content,
-        'image': image_path
-    }
-    blogs.append(new_blog)
-    return jsonify(new_blog), 201
+    new_blog = BlogPost(title=title, content=content, image=image_path)
+    db.session.add(new_blog)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_blog.id,
+        "title": new_blog.title,
+        "content": new_blog.content,
+        "image": new_blog.image
+    }), 201
 
 # Ruta para actualizar una entrada por su ID
 @app.route('/update_blog/<int:blog_id>', methods=['PUT'])
 def update_blog(blog_id):
-    blog = next((blog for blog in blogs if blog['id'] == blog_id), None)
+    blog = BlogPost.query.get(blog_id)
     if blog is None:
         return jsonify({'message': 'Blog no encontrado'}), 404
+
     data = request.form
-    blog['title'] = data['title']
-    blog['content'] = data['content']
+    blog.title = data['title']
+    blog.content = data['content']
 
     # Actualizar la imagen si se proporciona
     if 'image' in request.files:
         image = request.files['image']
         image.save(os.path.join('static/images', image.filename))
-        blog['image'] = f'static/images/{image.filename}'
+        blog.image = f'static/images/{image.filename}'
 
-    return jsonify(blog)
+    db.session.commit()
+
+    return jsonify({
+        "id": blog.id,
+        "title": blog.title,
+        "content": blog.content,
+        "image": blog.image
+    })
 
 # Ruta para borrar una entrada por su ID
 @app.route('/delete_blog/<int:blog_id>', methods=['DELETE'])
 def delete_blog(blog_id):
-    global blogs
-    blogs = [blog for blog in blogs if blog['id'] != blog_id]
+    blog = BlogPost.query.get(blog_id)
+    if blog is None:
+        return jsonify({'message': 'Blog no encontrado'}), 404
+
+    db.session.delete(blog)
+    db.session.commit()
+
     return jsonify({'message': 'Blog borrado correctamente'}), 200
 
 if __name__ == '__main__':
